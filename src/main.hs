e485451e17f3c58e63a819d4c798944252c69e9d
Yumenio <alejandroandy447@gmail.com>
2022-01-07 18:06:18 -0500

    pprinting board

--- src/main.hs
+++ src/main.hs
@@ -1,3 +1,5 @@
+import System.Random
+
 toStr :: [Int] -> String
 toStr [] = ""
 toStr (h:t) =
@@ -8,9 +10,15 @@ fill x times  | times == 1 = [x]
               | times < 1 = []
               | otherwise = [x] ++ fill x (times-1)
 
-init_table m n x  | m == 1 = [fill x n]
+init_board m n x  | m == 1 = [fill x n]
                   | m < 1 =  []
-                  | otherwise = [fill x n] ++ init_table (m-1) n x
+                  | otherwise = [fill x n] ++ init_board (m-1) n x
 
 
---xh
+pprint [] = putStrLn ""
+pprint (r:t) = do
+  print r
+  pprint t
+
+main :: IO ()
+main = let m = 3; n = 3; x = "0"; board = init_board m n x in print board
