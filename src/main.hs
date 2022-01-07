8ac958fa1c892970fd2df39fc28f0e33c12ac32d
Yumenio <alejandroandy447@gmail.com>
2022-01-06 21:29:39 -0500

    MxN table

--- src/main.hs
+++ src/main.hs
@@ -8,6 +8,6 @@ fill x times  | times == 1 = [x]
               | times < 1 = []
               | otherwise = [x] ++ fill x (times-1)
 
-init_table m n x  | m == 1 = fill x n
+init_table m n x  | m == 1 = [fill x n]
                   | m < 1 =  []
-                  | otherwise = fill x n ++ init_table (m-1) n x
+                  | otherwise = [fill x n] ++ init_table (m-1) n x
