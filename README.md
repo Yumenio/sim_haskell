38fb3fafe22c81835f4c5216d60479f4e13b48fe
Yumenio <alejandroandy447@gmail.com>
2022-01-06 21:22:00 -0500

    some haskell crappy code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sim_haskell
 Simulation+DeclarativeProgramming project of the 4th year of CS
+Second project of DeclarativeProgramming, and second project of the Simulation course as well
+
+Implementation of agents for simulating a house-cleaning robot, see **sim-prolog.pdf** for more details.
--- src/main.hs
+++ src/main.hs
@@ -1,1 +1,13 @@
-main = putStrLn "Hello, world!"
+toStr :: [Int] -> String
+toStr [] = ""
+toStr (h:t) =
+  let xh = show h
+  in xh ++ " " ++ toStr t
+
+fill x times  | times == 1 = [x]
+              | times < 1 = []
+              | otherwise = [x] ++ fill x (times-1)
+
+init_table m n x  | m == 1 = fill x n
+                  | m < 1 =  []
+                  | otherwise = fill x n ++ init_table (m-1) n x
