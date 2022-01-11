8eee9a010ed3f72dbf69d60c2bf301cc3234ebac
Yumenio <alejandroandy447@gmail.com>
2022-01-11 15:31:04 -0500

    before throwing all away and starting to work with randomR instead of randomRIO

--- src/main.hs
+++ src/main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 import System.Random
+import Distribution.Simple.Program.HcPkg (list)
 
 toStr :: [Int] -> String
 toStr [] = ""
@@ -9,38 +10,70 @@ toStr (h:t) =
 
 fill x times  | times == 1 = [x]
               | times < 1 = []
-              | otherwise = [x] ++ fill x (times-1)
+              | otherwise = x : fill x (times-1)
 
-init_board m n x  | m == 1 = [fill x n]
+initBoard m n x  | m == 1 = [fill x n]
                   | m < 1 =  []
-                  | otherwise = [fill x n] ++ init_board (m-1) n x
+                  | otherwise = fill x n : initBoard (m-1) n x
 
 
 -- generateObstacles :: [[Char]] -> [[Char]]
-generateObstacles :: [[Char]] -> IO [[Char]]
-generateObstacles board =
-  let m = length board; n = length $ board!!0; perc = 10; amount = div (m*n*perc) 100 in generateObstaclesAux board amount
+generateObstacles :: IO [[Char]] -> IO [[Char]]
+generateObstacles board = do
+  let m = rowCountIO board; n = colCountIO board; perc = 10
+  m_ <- m
+  n_ <- n
+  let amount = div (m_*n_*perc) 100 in generateObstaclesAux board amount
 
 -- generateObstaclesAux :: [[Char]] -> Int -> [[Char]]
-generateObstaclesAux :: [[Char]] -> Int -> IO [[Char]]
-generateObstaclesAux board amount | amount == 0 = return board
+generateObstaclesAux :: IO [[Char]] -> Int -> IO [[Char]]
+generateObstaclesAux board amount | amount == 0 = board
                                   | otherwise = do
-                                    let m = length board; n = length $ board!!0
-                                    x_ <- randomRIO (0,(m-1))
-                                    y_ <- randomRIO (0,(n-1))
-                                    let x = filterIO x_; y = filterIO y_; nboard = subNth0 board x y 'O' in generateObstaclesAux nboard (amount-1)
+                                    let m = rowCountIO board; n = colCountIO board
+                                    m_ <- m
+                                    n_ <- n
+                                    x_ <- randomRIO (0,m_-1)
+                                    y_ <- randomRIO (0,n_-1)
+                                    let x = filterIntIO x_; y = filterIntIO y_; nboard = subNth0 board x y 'O' in generateObstaclesAux nboard (amount-1)
                                     -- let (head, row:rs) = splitAt x board; (rhead, _:rtail) = splitAt y row; newrow = rhead++['O']++rtail in generateObstaclesAux (head++[newrow]++rs) (amount-1)
 
-subNth0 :: [[Char]] -> Int -> Int -> Char -> [[Char]]
-subNth0 board i j x = let (head, row:rs) = splitAt i board; (rhead, _:rtail) = splitAt j row; newrow = rhead++[x]++rtail in head++[newrow]++rs
+rowCountIO :: IO [[a]] -> IO Int 
+rowCountIO list = do
+  fmap length list
 
--- genBabyJail :: [[Char]] -> [[Char]]
--- genBabyJail board babyCount doneCount = if babyCount == doneCount then board else let (x,y) = getBoardIndex board doneCount; 
+colCountIO :: IO [[a]] -> IO Int 
+colCountIO list = do
+  escList <- list
+  let row = head escList
+  return $ length row
 
-getBoardIndex board straightLineIndex = let n = length $ board!!0; i = div straightLineIndex n; j = rem straightLineIndex n; in (i,j)
 
-filterIO :: Int -> Int
-filterIO x = x
+subNth0 :: IO [[Char]] -> Int -> Int -> Char -> IO [[Char]]
+subNth0 board i j x = do
+  escBoard <- board
+  let (head, row:rs) = splitAt i escBoard; (rhead, _:rtail) = splitAt j row; newrow = rhead++[x]++rtail in return $ head++[newrow]++rs
+
+genBabyJail :: IO [[Char]] -> Int -> Int -> IO [[Char]]
+genBabyJail board babyCount doneCount = if babyCount == doneCount then board else do 
+  (i,j) <- getBoardIndex board doneCount
+  let nboard = subNth0 board i j 'S' in genBabyJail nboard babyCount (doneCount+1)
+
+getBoardIndex :: IO [[Char]] -> Int -> IO (Int, Int)
+getBoardIndex board straightLineIndex = do
+  let n = colCountIO board;
+  escN <- n
+  let i = div straightLineIndex escN; j = rem straightLineIndex escN; in return (i,j)
+
+rowDim :: (Foldable f, Num b) => f a -> b
+rowDim = foldr (\ x -> (+) 1) 0
+
+filterIntIO :: Int -> Int
+filterIntIO x = x
+
+filterBoardIO :: IO [[Char]] -> [[Char]]
+filterBoardIO board = do
+  escBoard <- board
+  escBoard
 
 pprint [] = putStrLn ""
 pprint (r:t) = do
@@ -48,18 +81,18 @@ pprint (r:t) = do
   pprint t
 
 main :: IO ()
-main = let m = 3; n = 3; x = 'X'; board = init_board m n x in pprint board
+main = let m = 5; n = 5; x = 'X'; board = initBoard m n x; board' = generateObstacles board; board'' = genBabyJail board' 3 0 in pprint board''
 
-clean_perc_d board =
-  let m = length board; n = length $ board!!0 in clean_perc_w board 0 0 0 (m-1) (n-1)
+cleanPercD board =
+  let m = length board; n = length $ head board in cleanPercW board 0 0 0 (m-1) (n-1)
 
 -- clean_perc_w :: [[]]
-clean_perc_w board x y dirty m n  | x == m && y == n && board!!x!!y == 'C' = div ((dirty+1)*100) ((m+1)*(n+1))
+cleanPercW board x y dirty m n  | x == m && y == n && board!!x!!y == 'C' = div ((dirty+1)*100) ((m+1)*(n+1))
                                   | x == m && y == n = div (dirty*100) ((m+1)*(n+1))
-                                  | y == n && board!!x!!y == 'C' = clean_perc_w board (x+1) 0 (dirty+1) m n
-                                  | board!!x!!y == 'C' = clean_perc_w board x (y+1) (dirty+1) m n
-                                  | y == n = clean_perc_w board (x+1) 0 dirty m n
-                                  | otherwise = clean_perc_w board x (y+1) dirty m n
+                                  | y == n && board!!x!!y == 'C' = cleanPercW board (x+1) 0 (dirty+1) m n
+                                  | board!!x!!y == 'C' = cleanPercW board x (y+1) (dirty+1) m n
+                                  | y == n = cleanPercW board (x+1) 0 dirty m n
+                                  | otherwise = cleanPercW board x (y+1) dirty m n
 
 
 dirt :: [Char] -> Int
@@ -67,18 +100,18 @@ dirt [] = 0
 dirt ('C':t) = 1 + dirt t
 dirt (_:t) = 0 + dirt t
 
-dirt_perc :: [[Char]] -> Int
-dirt_perc board = let m = length board; n = length board in div ((sum $ map dirt board)*100) (m*n)
+dirtPerc :: [[Char]] -> Int
+dirtPerc board = let m = length board; n = length board in div (sum (map dirt board)*100) (m*n)
 
-clean_perc board = 100 - (dirt_perc board)
+cleanPerc board = 100 - dirtPerc board
 
-rnd_filter :: Int -> Int
-rnd_filter x = x
+rndFilter :: Int -> Int
+rndFilter x = x
 
 rrr :: IO ()
 rrr = do
   r <- randomRIO(1,100)
-  print $ rnd_filter r
+  print $ rndFilter r
 
 bnr :: IO Int
 bnr = let r = randomRIO(1,100) in r
