2bf5f0d27b90fc72d2ff25237bdaf0a649df5439
Yumenio <alejandroandy447@gmail.com>
2022-01-12 00:34:50 -0500

    randomly placing the clean-bot without overriding another non-empty cell

--- src/Main.hs
+++ src/Main.hs
@@ -13,16 +13,36 @@ generateObstacles board seed =
 
 generateObstaclesAux :: [[Char]] -> Int -> Int -> ([[Char]], Int)
 generateObstaclesAux board amount seed  | amount == 0 = (board, seed)
-                                        | otherwise = let m = length board; n = length $ head board; x = runRandom rand seed; y = runRandom rand x; xMod = rem x m; yMod = rem y n; nboard = subNth0 board xMod yMod 'O' in generateObstaclesAux nboard (amount-1) y 
+                                        | otherwise = let m = length board; n = length $ head board; x = runRandom rand seed; y = runRandom rand x; xMod = mod x m; yMod = mod y n; nboard = subNth0 board xMod yMod 'O' in generateObstaclesAux nboard (amount-1) y 
 
 genBabyJail :: [[Char]] -> Int -> Int -> [[Char]]
 genBabyJail board babyCount doneCount = if babyCount == doneCount then board else let (i,j) = getBoardIndex board doneCount; nboard = subNth0 board i j 'S' in genBabyJail nboard babyCount (doneCount+1)
 
-generateRobots :: [[Char]] -> Int -> Int -> [[Char]]
-generateRobots board amount seed = let m = length board; n = length $ head board; x = runRandom rand seed; xMod = rem x; y = runRandom rand x; xMod = rem y n in if board!!xMod!!yMod !! 'X' then generateRobots board amount y else let board' = subNth0 board x y 'R' in generateRobots board (amount-1) y
+generateRobots :: [[Char]] -> Int -> Int -> ([[Char]], Int)
+generateRobots board 0 seed = (board, seed)
+generateRobots board amount seed =
+  let
+    m = length board;
+    n = length $ head board;
+    x = runRandom rand seed; xMod = mod x m;
+    y = runRandom rand x; yMod = mod y n
+    in
+      if board!!xMod!!yMod /= 'X'
+        then generateRobots board amount y
+        else
+          let
+            board' = subNth0 board xMod yMod 'R'
+            in generateRobots board' (amount-1) y
 
 simulate :: Int -> Int -> Int -> IO()
-simulate m n seed = let x = 'X'; board = initBoard m n x; (board', seed') = generateObstacles board seed; board'' = genBabyJail board' 3 0 in pprint board''
+simulate m n seed =
+  let
+    x = 'X'; 
+    board = initBoard m n x;
+    (board', seed') = generateObstacles board seed;
+    board'' = genBabyJail board' 3 0
+    (board''', seed'') = generateRobots board'' 1 seed
+    in pprint board'''
 
 
 main :: IO ()
