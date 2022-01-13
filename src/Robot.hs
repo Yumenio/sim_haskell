d86912369d74571f72250cc5c2ae0de2be96214f
Yumenio <alejandroandy447@gmail.com>
2022-01-12 23:30:10 -0500

    robots now dont move to a obstructed cell

--- src/Robot.hs
+++ src/Robot.hs
@@ -22,42 +22,73 @@ moveLeftR board robot =
 
   -- let board' = subNth0 board i j 'X' in subNth0 board' i (j-1) 'R'
 
-moveUpR board 0 j = board
-moveUpR board i j = moveAny board i j (-1) 0 'X'
-
-moveRightR board i j =
+moveUpR :: [[Char]] -> Robot -> ([[Char]], Robot)
+moveUpR board robot =
   let
+    (i,j,s) = robotAll robot
+    in
+      if i == 0
+        then (board,robot)
+        else
+          let
+            board' = moveAny board i j (-1) 0 'X'
+            robot' = Robot (i-1) j s
+            in (board', robot')
+
+moveRightR :: [[Char]] -> Robot -> ([[Char]], Robot)
+moveRightR board robot =
+  let
+    (i,j,s) = robotAll robot
     m = length board
-    -- n = length $ head board
     in
-      if j == (m-1)
-        then board
-        else moveAny board i j 0 1 'X'
+      if i == m
+        then (board,robot)
+        else
+          let
+            board' = moveAny board i j 0 1 'X'
+            robot' = Robot i (j+1) s
+            in (board', robot')
 
-moveDownR board i j =
+moveDownR :: [[Char]] -> Robot -> ([[Char]], Robot)
+moveDownR board robot =
   let
-    -- m = length board
-    n = length $ head board
+    (i,j,s) = robotAll robot
+    m = length board
     in
-      if i == (n-1)
-        then board
-        else moveAny board i j 1 0 'X'
+      if i == (m-1)
+        then (board,robot)
+        else
+          let
+            board' = moveAny board i j 1 0 'X'
+            robot' = Robot (i+1) j s
+            in (board', robot')
 
 
 -- cleanR board i j = 
 
 moveAny board i j deltaI deltaJ sub =
-  let
-    oldItem = board!!i!!j;
-    board' = subNth0 board (i+deltaI) (j+deltaJ) oldItem
-    in subNth0 board' i j sub
+  if canMove board (i+deltaI) (j+deltaJ)
+    then
+      let
+        oldItem = board!!i!!j;
+        board' = subNth0 board (i+deltaI) (j+deltaJ) oldItem
+        in subNth0 board' i j sub
+    else
+      board
 
+
+canMove :: [[Char]] -> Int -> Int -> Bool
+canMove board i j =
+  let
+    elem = board!!i!!j
+    in  -- O => Obstacle, R => Robot, B => Baby in jail xd
+      not (elem == 'O' || elem == 'R' || elem == 'B')
 
 moveRobots :: [[Char]] -> [Robot] -> Int -> ([[Char]], [Robot], Int)
 moveRobots board [] seed = (board, [], seed)
 moveRobots board (robot: rs) seed =
   let
-    (board', robot') = moveLeftR board robot
+    (board', robot') = moveDownR board robot
     (board'', rs', seed') = moveRobots board' rs seed
     in (board'', robot':rs', seed')
 
